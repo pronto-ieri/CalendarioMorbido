835d746d8dade40dd7cf4773f7cca82603277b0c
thorongil05 <a.acquavia@studenti.unipi.com>
2026-06-24 20:09:01 +0200

    build: fix problem with DevJwtProducer

--- backend/src/main/java/it/calendariomorbido/auth/DevJwtProducer.java
+++ backend/src/main/java/it/calendariomorbido/auth/DevJwtProducer.java
@@ -1,6 +1,6 @@
 package it.calendariomorbido.auth;
 
-import io.quarkus.arc.profile.IfBuildProfile;
+import io.quarkus.arc.properties.IfBuildProperty;
 import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.enterprise.inject.Produces;
 import org.eclipse.microprofile.jwt.JsonWebToken;
@@ -11,7 +11,7 @@ import java.util.Set;
 public class DevJwtProducer {
 
     @Produces
-    @IfBuildProfile("dev")
+    @IfBuildProperty(name = "quarkus.oidc.enabled", stringValue = "false")
     public JsonWebToken produceDevJwt() {
         String devSubject = System.getProperty("dev.jwt.subject", "00000000-0000-0000-0000-000000000000");
         String devGroups = System.getProperty("dev.jwt.groups", "user");
