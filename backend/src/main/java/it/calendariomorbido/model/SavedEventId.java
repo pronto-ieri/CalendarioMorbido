fd51acc2e9de816d1e6a93c25d435bb24975e640
thorongil05 <a.acquavia@studenti.unipi.com>
2026-06-24 20:23:06 +0200

    chore: fix warning

--- backend/src/main/java/it/calendariomorbido/model/SavedEventId.java
+++ backend/src/main/java/it/calendariomorbido/model/SavedEventId.java
@@ -1,9 +1,22 @@
 package it.calendariomorbido.model;
 
 import java.io.Serializable;
+import java.util.Objects;
 import java.util.UUID;
 
 public class SavedEventId implements Serializable {
     public UUID userId;
     public UUID eventId;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof SavedEventId that)) return false;
+        return Objects.equals(userId, that.userId) && Objects.equals(eventId, that.eventId);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(userId, eventId);
+    }
 }
